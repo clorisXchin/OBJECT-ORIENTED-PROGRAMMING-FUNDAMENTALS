8e0f2c9faf377ebad2bb3ca0e0d0e636104550b4
yongxin-su <bernicesiew23@gmail.com>
2026-07-31 08:47:46 +0800

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class Customer {
+public class Customer extends User {
 
+	private List<String> BookingHistory;
+	
+	public Customer () {
+		super();
+		this.BookingHistory = new ArrayList<>();
+	}
+	
+	public Customer(String name, int age, String email, String password, String phone_number) {
+		super(name, age, email, password, phone_number);
+		this.BookingHistory = new  ArrayList<>();
+	}
+	
+	public void addBooking(String bookingDetails) {
+        this.BookingHistory.add(bookingDetails);
+	}
+        
+	public void viewHistory() {
+		System.out.println("\n--- Booking History for " + getName() + " ---");
+		if (BookingHistory.isEmpty())
+			System.out.println("No booking history found.");
+		else {
+			for(String booking: BookingHistory) {
+				System.out.println(booking);
+			}
+		}
+	}
+	
+	@Override
+	public void displayMenu() {
+	        System.out.println("\n==============================");
+	        System.out.println("   CINEMA TICKETING SYSTEM");
+	        System.out.println("==============================");
+	        System.out.println("1. View Movies");
+	        System.out.println("2. Book Ticket");
+	        System.out.println("3. Cancel Booking");
+	        System.out.println("4. View Booking");
+	        System.out.println("5. Exit");
+	        System.out.println("==============================");
+	        System.out.print("Enter your choice: ");
+
+	    
+		
+	}
 }
